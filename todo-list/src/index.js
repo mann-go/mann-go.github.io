b7f9ed19a3b902f832d44dffc90bcc80a01a8110
Mang-o <164549467+mann-go@users.noreply.github.com>
2025-01-12 18:41:39 +0000

    Added ability to add todo

--- todo-list/src/index.js
+++ todo-list/src/index.js
@@ -1,25 +1,48 @@
 import "./styles.css";
+import createTodo from "./modules/createTodo";
+import extractTodo from "./modules/extractTodoForm";
+import deleteTodo from "./modules/deleteTodo";
 
+/* ASIDE FUNCTIONS */
 const resizeButton = document.querySelector('[data-resize-btn]');
 resizeButton.addEventListener('click', function(e) {
     e.preventDefault();
     document.body.classList.toggle('sb-expanded');
 });
 
-var modal = document.getElementById('todo-modal');
-var btn = document.getElementById('add-todo');
+/* MODAL FUNCTIONS */
 var span = document.getElementsByClassName("close")[0];
 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-
 span.onclick = function() {
-    modal.style.display = "none";
+    submitTodoForm.style.display = "none";
 }
 
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (event.target == submitTodoForm) {
+        submitTodoForm.style.display = "none";
     }
 }
+
+/* TODO FUNCTIONALITY */
+const addTodoButton = document.getElementById('add-todo');
+addTodoButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    submitTodoForm.style.display = "block";
+});
+
+const submitTodoForm = document.getElementById('todo-add-modal');
+submitTodoForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const todoInfo = extractTodo();
+    createTodo(todoInfo.todoName, todoInfo.todoDesc, todoInfo.todoDueDate, todoInfo.todoPriority, todoInfo.todoNotes);
+
+    submitTodoForm.style.display = "none";
+
+});
+
+/* Doesn't really work, targets the first todo item */
+// const deleteTodoButton = document.getElementById('deleteTodoButton');
+// deleteTodoButton.addEventListener("click", (e) => {
+//     deleteTodo(e);
+// });
---
+++ todo-list/src/modules/deleteTodo.js
@@ -0,0 +1,11 @@
+// export default function deleteTodo(event) {
+    
+//     const confirmDelete = confirm('Are you sure you want to delete this todo item?');
+//     if(confirmDelete) {
+//         const gridItem = event.target.closest('.grid-item');
+//         if(gridItem) {
+//             gridItem.remove();
+//             console.log('Todo item deleted');
+//         }
+//     }
+// }
